de272bb1db5d42265c5c9993bff35f7c5c1379f8
Andreyka9999 <andrey@poddubnyak.com>
2024-08-15 19:57:33 +0300

    Created task KAN-6

---
+++ calculator.php
@@ -0,0 +1,29 @@
+<?php 
+/*  //Arithmetic operations 
+    // + - / * ** %
+*/
+
+$a = 5;        // variable a
+$b = 2;        // variable b
+$c = null;     // variable definition as 0
+
+$c = $a + $b;
+echo "Addtion operation result: $c \n";      
+
+$c = $a - $b;
+echo "Substraction operation result: $c \n";
+
+$c = $a / $b;
+echo "Division operation result: $c \n";
+
+$c = $a * $b; 
+echo "Multiplication operation result: $c \n";
+
+$c = $a ** $b;
+echo "Exponentiation operation result: $c \n";
+
+$c = $a % $b;
+echo "Moduls operation result: $c \n";
+
+
+?>
